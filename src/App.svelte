a756d09a85e763099f28f7fc8e0961fc5d49f2cb
Yonatan Hattav <yhattav@gmail.com>
2020-12-02 06:59:40 +0200

    after day 1

---
+++ src/App.svelte
@@ -0,0 +1,71 @@
+<script lang='ts'>
+  import {
+    Content,
+    Breadcrumb,
+    BreadcrumbItem,
+    Grid,
+    Row,
+    Column,
+    Tabs,
+    TabContent,
+    Tab,
+    Select,
+    PaginationNav,
+    SelectItem,
+  } from "carbon-components-svelte";
+  import { onMount, afterUpdate, setContext } from "svelte";
+
+  import Header from "./components/Header.svelte";
+  import Theme from "./components/Theme.svelte";
+  import Page from "./components/Page.svelte";
+  import Day1 from "./days/day1/Day1.svelte";
+
+
+  let theme: "g10" = "g10";
+
+  setContext('Pagination', {
+    handlePageChange: (pageIndex: number) => {
+      pageChanged(pageIndex);
+    },
+    getLocalPage,
+    getCurrentPage,
+  })
+  let currentPageIndex = getLocalPage();
+  console.log(currentPageIndex);
+  function getLocalPage() {
+    console.log(window.localStorage.aocLocalPage);
+    return Number(window.localStorage.aocLocalPage || 0);
+  }
+  function pageChanged(pageIndex: number) {
+    console.log('pageChanged', pageIndex);
+    if (typeof pageIndex == "number") {
+      window.localStorage.aocLocalPage = pageIndex;
+    currentPageIndex = pageIndex;
+    }
+  }
+  function getCurrentPage() {
+    return currentPageIndex;
+  }
+  // function loadPage(pageIndex: numebr) {
+  //   // import(`./days/day${pageIndex+1}/Day${pageIndex+1}.svelte`).then(module=>{
+  //   import(`./days/day1/Day1.svelte`).then((module)=>{
+  //     PageComponent = module.default;
+  //   }).catch(error=>{
+  //     console.error(error);
+  //   });
+  // };
+  // $: loadPage(currentPageIndex);
+</script>
+
+<Theme persist bind:theme>
+  <Header />
+  <Content style="background: none; padding: 1rem">
+    <Grid>
+      <Row>
+        <Column noGutter>
+        <Page {currentPageIndex}/>
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
+</Theme>
---
+++ src/days/day1/Day1.svelte
@@ -0,0 +1,84 @@
+<script>
+  import { Toggle } from "carbon-components-svelte";
+
+    import {exampleArray, inputArray} from './day1Input.ts'
+
+    async function findThree() {
+       return await usedInput.map(initialValue=>{
+            return usedInput.map((value,index)=>findPair(index,initialValue));
+        })
+    };
+
+    async function findTwo() {
+            return await usedInput.map((value,index)=>findPair(index));
+    };
+
+    function findPair(index, initialVal = 0) {
+        const a = usedInput[index];
+        const bIndex = usedInput.findIndex((value)=>{
+            const sum = a + value;
+            const isAMatch = (value + a) === 2020 - initialVal;
+            return isAMatch;
+            });
+        if (bIndex > -1) {
+            const b = usedInput[bIndex];
+            // console.warn(a , index, b, index, a * b, a*b*initialVal);
+            res = initialVal ? a*b*initialVal : a*b;
+        }
+    };
+
+    let res='no answer yet';
+    let time = 'X';
+    let initUseExample = window.localStorage.aocUseExample;
+    let usedInput = initUseExample ? exampleArray : inputArray;
+    let toggled = initUseExample === 'true';
+    let useExample;
+
+    async function script2() {
+        let _res = await findThree();
+        return _res;
+    }
+    async function script1() {
+        let _res = await findTwo();
+        return _res;
+    }
+
+    function toggleInput(event) {
+        useExample = event.target.checked;
+        window.localStorage.aocUseExample = useExample;
+    }
+
+    async function run(part) {
+        const started = Date.now()
+        if(part === 1){ 
+            await script1();
+        }
+        if(part === 2){
+            await script2();
+        }
+        const ended = Date.now();
+        time = ended - started + 'ms'
+    }
+
+$: usedInput = useExample ? exampleArray : inputArray;
+
+</script>
+1
+<p>  
+    <Toggle labelText="Use Example Input" {toggled} on:change={toggleInput}/>
+</p>
+<p>
+    <button on:click={()=>{run(1)}}>Run1</button>
+    <button on:click={()=>{run(2)}}>Run2</button>
+</p>
+<p>    
+    {res}
+</p>
+<p>
+in {time};
+</p>
+
+
+<style>
+
+</style>
