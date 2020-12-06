f48068dae36355618f529fe49cc677cd937bf0f3
Yonatan Hattav <yhattav@gmail.com>
2020-12-06 08:23:30 +0200

    day 6

--- src/days/day6/day6script.ts
+++ src/days/day6/day6script.ts
@@ -3,12 +3,68 @@ import {exampleArray, inputArray} from './day6Input'
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    const regex = /,,/gm;
+const regex2 =  /,/gm;
+    _res = countThem(usedInput.replace(regex,'=').replace(regex2,'').split('=').map(value=>value.split('')));
     return _res;
 }
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    const regex = /,,/gm;
+const regex2 =  /,/gm;
+    _res = countThem2(usedInput.replace(regex,'=').split('=').map(value=>value.split(',')));
     return _res;
+}
+
+function countThem(inputArray) {
+    let res = 0;
+    inputArray.forEach(group=>{
+        res += countUnique(group);
+    });
+    return res;
+}
+
+function countUnique(iterable) {
+    return new Set(iterable).size;
+  }
+
+function countThem2(inputArray) {
+    let res = 0;
+    inputArray.forEach(group=>{
+        res += countEveryoneInGroup(group);
+    });
+    return res;
+}
+
+// function countEveryoneInGroup(groupArray){
+//     console.log(groupArray)
+//     groupArray.map(group=>{
+//         return new Set(group)
+//     })
+//     console.log(groupArray)
+//     debugger
+//     const pplInGroup = groupArray.length;
+//     let allAnswers = groupArray.join('').split('').sort().join('');
+//     console.log(allAnswers);
+//     let str1 = `[abcdefghijklmnopqrstuvwxyz]{${pplInGroup}}`
+//     console.log(str1)
+//     var re = new RegExp(str1, "gm");
+//     let matches = allAnswers.match(re);
+//     debugger;
+//     const reducer = (accumulator, currentValue) => accumulator + (countUnique(currentValue) === 1 ? 1 : 0);
+//     let num = matches.reduce(reducer, 0);
+//     return num;
+// }
+
+
+function countEveryoneInGroup(groupArray){
+    let res = 0;
+    for (let i = 97; i <= 122; i++) {
+        let tested = String.fromCharCode(i)
+        let reducer = (accumulator, currentValue) => accumulator + (currentValue.split('').some(value => value === tested) ? 1 : 0);
+        if (groupArray.reduce(reducer, 0) === groupArray.length) res++;
+        
+    }
+    return res;
 }
