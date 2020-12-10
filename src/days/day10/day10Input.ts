5d792f8357dfd8f79dbef42332ae256b0b6e61e0
Yonatan Hattav <yhattav@gmail.com>
2020-12-10 11:04:25 +0200

    day10

--- src/days/day10/day10Input.ts
+++ src/days/day10/day10Input.ts
@@ -1,14 +1,14 @@
 // const example = '';
-// const example = '';
-const example = '';
-const input = '';
+const example = '28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3';
+// const example = '16,10,15,5,1,11,7,19,6,12,4';
+const input = '8,40,45,93,147,64,90,125,149,145,111,126,9,146,38,97,103,6,122,34,18,35,96,86,116,29,59,118,102,26,66,17,74,94,5,114,128,1,75,47,141,58,65,100,63,12,53,25,106,136,15,82,22,117,2,80,79,139,7,81,129,19,52,87,115,132,140,88,109,62,73,46,24,69,101,110,16,95,148,76,135,142,89,50,72,41,39,42,56,51,57,127,83,121,33,32,23';
 // const regex = /contain/gm;
 // const regex2 = /bags|bag/gm;
 // const regex3 = /^ | $/gm;
 
 
-const exampleArray = example;
+const exampleArray = example.split(',').map(Number).sort((a, b) => a - b);
 console.log(exampleArray)
-const inputArray = input;
+const inputArray = input.split(',').map(Number).sort((a, b) => a - b);
 
 export {exampleArray, inputArray};
--- src/days/day10/day10script.ts
+++ src/days/day10/day10script.ts
@@ -2,13 +2,66 @@ import {exampleArray, inputArray} from './day10Input'
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    _res = countGaps(usedInput);
     return _res;
 }
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    _res = findNumberOfPaths2(usedInput);
     return _res;
 }
 
+function countGaps(inputArray) {
+    let last = 0;
+    let bucket = [0,0,1];
+    let foundError = false;
+    inputArray.forEach(value => {
+        let gap = value - last;
+        if(gap > 3){
+            foundError = true;
+        } else {
+            bucket[gap-1]++;
+            last = value;
+        }
+    });
+    if(foundError) return 'error';
+    return bucket[0]*bucket[2];
+}
+
+
+function findPossibilities(inputArray,pointer){
+    let possibilities = [];
+    let startValue = inputArray[pointer];
+    let start = pointer;
+    do {
+        pointer++;
+        if(inputArray[pointer] - startValue <= 3) {
+            possibilities.push(pointer);
+        }
+    } while (pointer - start < 4);
+    return possibilities;
+}
+//19208
+function findNumberOfPaths2(inputArray){
+    let array = inputArray.slice(0);
+    array.unshift(0);
+    array.push(inputArray[inputArray.length - 1]+3);
+
+    let possibilitiesArray = [];
+    array.forEach((value,index)=>{
+        let res = findPossibilities(array,index).length;
+        possibilitiesArray.push(res);
+    })
+    possibilitiesArray.pop();
+    possibilitiesArray.unshift(1);
+    possibilitiesArray.push(1);
+    let str = possibilitiesArray.join('')
+    var re7 = new RegExp('13321',"g");
+    var re4 = new RegExp('1321',"g");
+    var re2 = new RegExp('121',"g");
+    const matches7 = (str.match(re7)).length;
+    const matches4 = (str.match(re4)).length;
+    const matches2 = (str.match(re2)).length;
+    return Math.pow(7,matches7)*Math.pow(4,matches4)*Math.pow(2,matches2);
+}
