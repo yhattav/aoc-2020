76f2f637bde0376c5051e8c52826856b82595578
Yonatan Hattav <yhattav@gmail.com>
2020-12-10 11:46:37 +0200

    but of simplifying and protection for day10

--- src/days/day10/day10script.ts
+++ src/days/day10/day10script.ts
@@ -8,7 +8,7 @@ export async function script1(useExample: boolean) {
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = findNumberOfPaths2(usedInput);
+    _res = findNumberOfPaths(usedInput);
     return _res;
 }
 
@@ -43,25 +43,23 @@ function findPossibilities(inputArray,pointer){
     return possibilities;
 }
 //19208
-function findNumberOfPaths2(inputArray){
-    let array = inputArray.slice(0);
+function findNumberOfPaths(sortedArray){
+    let array = sortedArray.slice(0);
     array.unshift(0);
-    array.push(inputArray[inputArray.length - 1]+3);
-
-    let possibilitiesArray = [];
-    array.forEach((value,index)=>{
-        let res = findPossibilities(array,index).length;
-        possibilitiesArray.push(res);
-    })
-    possibilitiesArray.pop();
-    possibilitiesArray.unshift(1);
-    possibilitiesArray.push(1);
-    let str = possibilitiesArray.join('')
+    array.push(sortedArray[sortedArray.length - 1]+3);
+    array = array.map((value,index) => findPossibilities(array,index).length);
+    array.pop();
+    array.unshift(1);
+    array.push(1);
+    let str = array.join('')
     var re7 = new RegExp('13321',"g");
     var re4 = new RegExp('1321',"g");
     var re2 = new RegExp('121',"g");
-    const matches7 = (str.match(re7)).length;
-    const matches4 = (str.match(re4)).length;
-    const matches2 = (str.match(re2)).length;
-    return Math.pow(7,matches7)*Math.pow(4,matches4)*Math.pow(2,matches2);
+    const matches7 = str.match(re7);
+    const num7 = matches7 && matches7.length;
+    const matches4 = str.match(re4);
+    const num4 = matches4 && matches4.length;
+    const matches2 = str.match(re2);
+    const num2 = matches2 && matches2.length;
+    return Math.pow(7,num7)*Math.pow(4,num4)*Math.pow(2,num2);
 }
