3637136fb03b907340f4ca2a686c3882184c460c
Yonatan Hattav <yhattav@gmail.com>
2020-12-13 20:04:49 +0200

    day13 al last

--- src/days/day13/day13Input.ts
+++ src/days/day13/day13Input.ts
@@ -1,14 +1,22 @@
 // const example = '';
 // const example = '';
-const example = '';
-const input = '';
+const example = '939,7,13,x,x,59,x,31,19';
+// const example = '939,67,7,59,61';
+// const example = '939,17,x,13,19';
+// const example = '939,67,7,59,61';
+// const example = '939,67,x,7,59,61';
+// const example = '939,67,7,x,59,61';
+// const example = '939,1789,37,47,1889';
+const input = '1000340,13,x,x,x,x,x,x,37,x,x,x,x,x,401,x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,x,x,x,19,x,x,x,23,x,x,x,x,x,29,x,613,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41';
 // const regex = /contain/gm;
 // const regex2 = /bags|bag/gm;
 // const regex3 = /^ | $/gm;
 
 
-const exampleArray = example;
+const exampleArray = example.split(',').map(Number);
+// exampleArray[120] = 221;
 console.log(exampleArray)
-const inputArray = input;
+
+const inputArray = input.split(',').map(Number);
 
 export {exampleArray, inputArray};
--- src/days/day13/day13script.ts
+++ src/days/day13/day13script.ts
@@ -2,13 +2,73 @@ import {exampleArray, inputArray} from './day13Input'
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    _res = getNearest(usedInput);
     return _res;
 }
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    _res = findOrder(usedInput);
     return _res;
 }
 
+function getNearest(inputArray) {
+    let array = inputArray.slice(0);
+
+    let santaTime = array.shift();
+    let smallest = Infinity;
+    let smallestIdx;
+    array.map((value,index)=>{
+        let res = value - santaTime % value;
+        if(smallest > res) {smallest = res; smallestIdx = index;}
+        return res;
+    })
+    return smallest*array[smallestIdx];
+}
+
+
+function findOrder(inputArray) {
+    let array = inputArray.slice(0);
+    array.shift();
+    let object = {};
+    array.map((value,index)=>{
+        if (value > 0){object[value] = index};
+    })
+    let res;
+    let shouldContinue = true;
+    do {
+        const {should,finalAnswer,busNumber} = killTwo2(object);
+        shouldContinue = should;
+        res = finalAnswer;
+    } while (shouldContinue);
+    return (res);
+}
+function killTwo2(givenObject) {
+    let numbers = []
+    numbers = Object.keys(givenObject).reverse();
+    numbers = numbers.slice(0,2);
+    
+    let first = numbers[0];
+    let firstIdx = givenObject[first];
+    if (numbers.length===1) return {should:false, finalAnswer: numbers[0]-givenObject[numbers[0]]};
+    
+    let res;
+    let start = numbers.reduce((a, b)=> a*b, 1) - firstIdx;
+    for (let i = start; i > 0; i-=first) {
+        let match = false;
+        match = numbers.every(val=>{
+            let index = givenObject[val];
+            let num = (i + index);
+            res = num % val === 0
+            return res;
+        })
+        if (match === true) {res = i; break;}
+    }
+
+    let newIndex = start - res + firstIdx;
+    let busNumber = start + firstIdx;
+    delete givenObject[numbers[0]];
+    delete givenObject[numbers[1]];
+    givenObject[busNumber] = newIndex;
+    return ({should:true, finalAnswer: newIndex, busNumber});
+}
