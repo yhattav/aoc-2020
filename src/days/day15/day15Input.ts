7760314750e1e9908e7c632a6d29ff1541345a83
Yonatan Hattav <yhattav@gmail.com>
2020-12-16 07:00:49 +0200

    day15

--- src/days/day15/day15Input.ts
+++ src/days/day15/day15Input.ts
@@ -1,14 +1,13 @@
 // const example = '';
-// const example = '';
-const example = '';
-const input = '';
+const example = '0,3,6';
+const input = '1,2,16,19,18,0';
 // const regex = /contain/gm;
 // const regex2 = /bags|bag/gm;
 // const regex3 = /^ | $/gm;
 
 
-const exampleArray = example;
+const exampleArray = example.split(',').map(Number);
 console.log(exampleArray)
-const inputArray = input;
+const inputArray = input.split(',').map(Number);
 
 export {exampleArray, inputArray};
--- src/days/day15/day15script.ts
+++ src/days/day15/day15script.ts
@@ -2,13 +2,37 @@ import {exampleArray, inputArray} from './day15Input'
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    _res = run(usedInput,2020);
     return _res;
 }
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = (usedInput);
+    _res = run(usedInput,30000000);
     return _res;
 }
+function run(inputArray, numOfTimes: number) {
+    let startinglength = inputArray.length;
+    let elfTurn = startinglength;
+    let lastNumber
+    let record = new Map();
+    let recordArray = inputArray.slice(0,inputArray.length-1);
+    recordArray.forEach((value,index)=>{
+        record.set(value, index+1);
+    })
+    lastNumber = inputArray[inputArray.length - 1];
+    do {
+        let temp;
+        if(record.has(lastNumber)) {
+            temp = elfTurn  - record.get(lastNumber);
+        } else {
+            temp = 0;
+        }
+        record.set(lastNumber, elfTurn);
+        lastNumber = temp;
+    elfTurn++;
+    } while (elfTurn<numOfTimes);
+    return lastNumber
+}
+
 
