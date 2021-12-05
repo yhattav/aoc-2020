89ffbbf313b48c1deb08857b0df48bb8af28783a
Yonatan Hattav <yhattav@gmail.com>
2021-12-05 20:06:46 +0200

    prep day6

--- src/pages.js
+++ src/pages.js
@@ -32,5 +32,6 @@ export const pages = {
     "3":  () => import(`./days/2021/day3/Day3.svelte`).catch(e=>console.error(e)),
     "4":  () => import(`./days/2021/day4/Day4.svelte`).catch(e=>console.error(e)),
     "5":  () => import(`./days/2021/day5/Day5.svelte`).catch(e=>console.error(e)),
+    "6":  () => import(`./days/2021/day6/Day6.svelte`).catch(e=>console.error(e)),
   }
 };
