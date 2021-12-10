a654423f00958470463c2714a68c71416b00c28b
Yonatan Hattav <yhattav@gmail.com>
2021-12-10 20:20:01 +0200

    day10

--- src/days/2021/day10/day10script.ts
+++ src/days/2021/day10/day10script.ts
@@ -1,4 +1,13 @@
-import {exampleArray, inputArray} from './day10Input'
+import {exampleArray, inputArray, valueMap, valueMap2} from './day10Input'
+let reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const regs = [
+    /\[]/,
+    /\(\)/,
+    /{}/,
+    /<>/,
+]
+
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
@@ -13,8 +22,70 @@ export async function script2(useExample: boolean) {
 }
 
 const run = (input) => {
-    
+    let newArr = input.slice().map((line)=>{
+        let str = line;
+        let matched:boolean;
+        do {
+            matched = regs.some(reg => {
+                const match = str.match(reg);
+                if(match) {
+                    let aaa =  str.split('');
+                    aaa.splice(match.index,2);
+                    str = aaa.join('');
+                }
+                return !!match;
+            })
+        } while(matched);
+        let arr = str.split('')
+        let indexx = arr.findIndex(val=>{
+            return ['>','}',')',']'].some(char=>char===val)
+        })
+        if(indexx > 0) {
+            return valueMap[arr[indexx]];
+        } else {
+            return 0;
+        }
+    })
+    return newArr.reduce(reducer)
 }
-const run2 = (input) => {
 
+const run2 = (input) => {
+    let newArr = input.slice().map(removeAllValidBlocks)
+
+    let onlyIncomplete = newArr.filter(line=>{
+        return !line.split('').some(val=>['>','}',')',']'].some(char=>char===val));
+    })
+    let scores = onlyIncomplete.map(line=>{
+        let reversedArr = line.split('').reverse();
+        let acc = 0;
+        reversedArr.forEach(value=>{
+            acc*=5;
+            acc+=valueMap2[value];
+        })
+        return acc
+    });
+
+    let sorted = scores.sort(function(a, b) {
+        return a - b;
+    });
+
+    return sorted[Math.floor(sorted.length/2)]
+}
+
+
+const removeAllValidBlocks = (line)=>{
+    let str = line;
+    let matched:boolean;
+    do {
+        matched = regs.some(reg => {
+            const match = str.match(reg);
+            if(match) {
+                let aaa =  str.split('');
+                aaa.splice(match.index,2);
+                str = aaa.join('');
+            }
+            return !!match;
+        })
+    } while(matched);
+    return str;
 }
--- src/days/gen/day10script.ts
+++ src/days/gen/day10script.ts
@@ -1,4 +1,26 @@
 import {exampleArray, inputArray} from './day10Input'
+const regexs = {
+    '(': /([(])[^()]+[)]/,
+    '[': /([[]])[^()]+[)]/,
+    '<': /([<])[^()]+[)]/,
+    '{': /([{}])[^()]+[)]/,
+}
+
+let pairs = [
+    ['{','}'],
+    ['(',')'],
+    ['[',']'],
+    ['<','>'],
+]
+
+let regs = [
+    /\[]/,
+    /\(\)/,
+    /{}/,
+    /<>/,
+]
+
+
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
@@ -13,8 +35,31 @@ export async function script2(useExample: boolean) {
 }
 
 const run = (input) => {
-    
+    let newArr = input.slice().map(line=>{
+        let matched = false;
+        debugger;
+        do {
+            regs.forEach(reg => {
+                const match = line.match(reg);
+                debugger;
+            })
+        } while(matched);
+
+    })
 }
 const run2 = (input) => {
 
+}
+
+function removeSingleParentheses(inputString, calcParentheslessString) {
+    const regex = /([(])[^()]+[)]/
+    const match = inputString.match(regex);
+    if (match) {
+        // console.warn(match);
+        const toCalc = match[0].slice(1,-1);
+        const replaced = calcParentheslessString(toCalc);
+        return inputString.replace(match[0], replaced);
+    } else {
+        return inputString;
+    }
 }
