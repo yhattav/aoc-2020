80f6a1252cb94c220aa5a945a9074cf367aa88ea
Yonatan Hattav <yhattav@gmail.com>
2021-12-14 08:52:23 +0200

    day14

--- src/days/2021/day14/day14Input.ts
+++ src/days/2021/day14/day14Input.ts
@@ -1,14 +1,14 @@
 // const example = '';
 // const example = '';
-const example = '';
-const input = '';
+const example = 'NNCB|CH -> B,HH -> N,CB -> H,NH -> C,HB -> C,HC -> B,HN -> C,NN -> C,BH -> H,NC -> B,NB -> B,BN -> B,BB -> N,BC -> B,CC -> N,CN -> C';
+const input = 'BNBBNCFHHKOSCHBKKSHN|CH -> S,KK -> V,FS -> V,CN -> P,VC -> N,CB -> V,VK -> H,CF -> N,PO -> O,KC -> S,HC -> P,PP -> B,KO -> B,BK -> P,BH -> N,CC -> N,PC -> O,FK -> N,KF -> F,FH -> S,SS -> V,ON -> K,OV -> K,NK -> H,BO -> C,VP -> O,CS -> V,KS -> K,SK -> B,OP -> S,PK -> S,HF -> P,SV -> P,SB -> C,BC -> C,FP -> H,FC -> P,PB -> N,NV -> F,VO -> F,VH -> P,BB -> N,SF -> F,NB -> K,KB -> S,VV -> S,NP -> N,SO -> O,PN -> B,BP -> H,BV -> V,OB -> C,HV -> N,PF -> B,SP -> N,HN -> N,CV -> H,BN -> V,PS -> V,CO -> S,BS -> N,VB -> H,PV -> P,NN -> P,HS -> C,OS -> P,FB -> S,HO -> C,KH -> H,HB -> K,VF -> S,CK -> K,FF -> H,FN -> P,OK -> F,SC -> B,HH -> N,OH -> O,VS -> N,FO -> N,OC -> H,NF -> F,PH -> S,HK -> K,NH -> H,FV -> S,OF -> V,NC -> O,HP -> O,KP -> B,BF -> N,NO -> S,CP -> C,NS -> N,VN -> K,KV -> N,OO -> V,SN -> O,KN -> C,SH -> F';
 // const regex = /contain/gm;
 // const regex2 = /bags|bag/gm;
 // const regex3 = /^ | $/gm;
 
 
-const exampleArray = example;
+const exampleArray = example.split('|');
 console.log(exampleArray)
-const inputArray = input;
+const inputArray = input.split('|');
 
 export {exampleArray, inputArray};
--- src/days/2021/day14/day14script.ts
+++ src/days/2021/day14/day14script.ts
@@ -1,17 +1,122 @@
 import {exampleArray, inputArray} from './day14Input'
+import _ from 'lodash'
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = run(usedInput);
+    _res = run2(usedInput,10);
     return _res;
 }
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = run(usedInput);
+    _res = run2(usedInput,40);
     return _res;
 }
 
-const run = (input) => {
-
+const parseInput = (input: [any,any]) =>{
+    let poly = input[0];
+    //CH -> B
+    let pairs = input[1].split(',').map(el=>el.split(' -> '));
+    return {poly,pairs}
 }
+
+const run2 = (input,steps=40)=>{
+    let {poly,pairs} = parseInput(input);
+    let objectPoly = parsePoly(poly);
+    let counter = 1;
+    while (counter <= steps) {
+        objectPoly = step2(objectPoly, pairs)
+        counter++;
+    }  
+    return formAnswer(objectPoly.singleChars)
+}
+
+const parsePoly = (poly)=>{
+    let res = {singleChars:{}}
+    for(var i=0; i<poly.length-1;i++) {
+            res[poly[i]+poly[i+1]] = res[poly[i]+poly[i+1]] ? res[poly[i]+poly[i+1]] + 1 : 1;
+    }
+    for(var i=0; i<=poly.length-1;i++) {
+            res.singleChars[poly[i]] = res.singleChars[poly[i]] ? res.singleChars[poly[i]] + 1 : 1;
+    }
+    return res
+}
+
+
+
+const step2 =  (objectPoly,pairs) => {
+    const newObjectPoly = _.cloneDeep(objectPoly)
+    pairs.forEach(pair=>{
+        let numOfOccurences = objectPoly[pair[0]];
+        if(numOfOccurences) {
+            if(newObjectPoly.singleChars[pair[1]]) {
+                newObjectPoly.singleChars[pair[1]]+=numOfOccurences;
+            } else {
+                newObjectPoly.singleChars[pair[1]]=numOfOccurences;
+            }
+            let a = pair[0][0] + pair[1];
+            let b =  pair[1] + pair[0][1];
+            newObjectPoly[pair[0]]-=numOfOccurences;
+            newObjectPoly[a] = newObjectPoly[a] ? newObjectPoly[a] + numOfOccurences : numOfOccurences;
+            newObjectPoly[b] = newObjectPoly[b] ? newObjectPoly[b] + numOfOccurences : numOfOccurences;
+        }
+    })
+    return newObjectPoly
+}
+
+
+const formAnswer=(chars:object) =>{
+    let smallest = Infinity;
+    let largest = 0;
+    Object.values(chars).forEach((value:number)=>{
+        smallest = Math.min(smallest,value);
+        largest = Math.max(largest,value);
+    })
+    return largest - smallest
+}
+
+
+// const run = (input,steps=40) => {
+//     let {poly,pairs} = parseInput(input);
+//     console.log(poly,pairs)
+//     let counter = 1;
+//     let newPoly = poly
+//     while (counter <= steps) {
+//         //step
+//         newPoly = step(newPoly, pairs)
+//         counter++;
+//     }  
+//     return count(newPoly);
+// }
+
+// const step =  (poly,pairs) => {
+//     let acc = 0;
+//     let newPoly = poly;
+//     let instructions = [];
+//     pairs.forEach(pair => {
+//     for(var i=0; i<poly.length-1;i++) {
+//         if (poly[i]+poly[i+1] === pair[0]) {
+//             instructions.push([i+1,pair[1]])
+
+//         }
+//     }
+// })
+// instructions = instructions.sort(function(a, b) {
+//     return a[0] - b[0];
+//   });
+//   console.log({instructions})
+//   instructions.forEach(instruction =>{
+//             newPoly = newPoly.slice(0, instruction[0]+acc) + instruction[1] + newPoly.slice(instruction[0]+acc);
+//             acc++;
+//   });
+//     console.log({newPoly});
+//     return newPoly;
+// }
+
+// let count = (poly)=>{
+//     let chars = {};
+//     for(var i=0; i<=poly.length-1;i++) {
+//         chars[poly[i]] = chars[poly[i]] ? (chars[poly[i]]+1) : 1
+//     }
+//     console.log(chars);
+// }
