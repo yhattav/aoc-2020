6812c696f798785a7cf2ed27350eb029f7db2093
Yonatan Hattav <yhattav@gmail.com>
2022-12-10 17:14:58 +0200

    days 9 and 5

--- src/days/2022/day5/day5script.ts
+++ src/days/2022/day5/day5script.ts
@@ -1,5 +1,5 @@
 
-    import {exampleArray, inputArray} from './day5input'
+import {exampleArray, inputArray} from './day5input'
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
@@ -9,12 +9,184 @@ export async function script1(useExample: boolean) {
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = run(usedInput);
+    _res = run2(usedInput);
     return _res;
 }
 
 export const run = (input) => {
-
+    return parseInput(input);
+}
+export const run2 = (input) => {
+    return parseInput2(input);
 }
 
-      
+const parseInput2 = (input) => {
+    /* create a new array from the beggining of input up to the element that contains an empty string */
+    let stacks = input.slice(0,input.indexOf(''));
+    /* create a new array from the element after the empty string to the end of the input */
+    const commands = input.slice(input.indexOf('')+1);
+    /* log the two arrays */
+    console.log(stacks, commands)
+    /* reverse the stacks array */
+    stacks = stacks.reverse();
+    /* split each element in the stacks array into an array of characters */
+    stacks = stacks.map((stack)=>stack.split(''));
+    /* for each index in the first element of stacks array where there is a number, create a new array with the characters from the rest of the elements in the stacks array at that index */
+    stacks[0] = stacks[0].map((char, index)=>{
+        if(!isNaN(parseInt(char))) {
+            return stacks.map((stack)=>stack[index])
+        }
+    })
+    console.log(stacks)
+    /* remove all the undefined elements from the first element of the stacks array */
+    stacks[0] = stacks[0].filter((char)=>char !== undefined)
+    /*copy the first element in stacks to a new array */
+    let gameStacks = stacks[0].slice();
+    /* pop elements from each gameStack in gameStacks that are empty strings */
+    gameStacks.map((stack)=>{
+        while(stack[stack.length-1] === ' ') {
+            stack.pop();
+        }   
+    })
+    /* shift each gameStack once */
+    gameStacks.map((stack)=>{
+        stack.shift();
+    })
+
+    // debugger;
+    /* for each element in the commands array, split the element into an array of characters */
+    commands.map((command, index)=>{
+        commands[index] = command.split('');
+    })
+    /* for each element in the commands array, keep only numbers */
+    commands.map((command, index)=>{
+        commands[index] = command.filter((char)=>!isNaN(parseInt(char)));
+    })
+
+
+    /* for each element in the commands array, convert each character in the element to a number */
+    commands.map((command, index)=>{
+        commands[index] = command.map((char)=>parseInt(char));
+    })
+    /* in commands , subtract 1 from the last element and the one before it */
+    commands.map((command, index)=>{
+        command[command.length-1] -= 1;
+        command[command.length-2] -= 1;
+    })
+
+    /* for each element in the commands array,  create an object where key to is the last element, key from is the one bofore it and key amount is the rest of the elements joined together */
+    commands.map((command, index)=>{
+        commands[index] = {
+            to: command[command.length-1],
+            from: command[command.length-2],
+            amount: Number(command.slice(0,command.length-2).join(''))
+        }
+    })
+    /* for each element in the commands array, create an array where the first element is the amount, the second is the from and the third is the to */
+    commands.map((command, index)=>{
+        commands[index] = [command.amount, command.from, command.to];
+    })
+    /* for each command in the commands array, move the amount stated in the first element in the command elemnts from the gameStack at the index stated at the second element to the index stated at the third element*/
+    commands.map(([amount, from, to])=>{
+        /* move amount elements from the gameStack at from to the game stack at to */
+        let clamps = [];
+
+        for(let i = 0; i < amount; i++) {
+            clamps.push(gameStacks[from].pop());
+        }
+        clamps = clamps.reverse();
+        gameStacks[to] = gameStacks[to].concat(clamps);
+    })
+
+    /* combine a string out of the last character element in each gameStacks element */
+    let string = gameStacks.map((stack)=>stack[stack.length-1]).join('');
+    /* log the string */
+    console.log(string)
+    return string;
+
+    /* log the stacks array */
+    console.log(gameStacks)
+}
+const parseInput = (input) => {
+    /* create a new array from the beggining of input up to the element that contains an empty string */
+    let stacks = input.slice(0,input.indexOf(''));
+    /* create a new array from the element after the empty string to the end of the input */
+    const commands = input.slice(input.indexOf('')+1);
+    /* log the two arrays */
+    console.log(stacks, commands)
+    /* reverse the stacks array */
+    stacks = stacks.reverse();
+    /* split each element in the stacks array into an array of characters */
+    stacks = stacks.map((stack)=>stack.split(''));
+    /* for each index in the first element of stacks array where there is a number, create a new array with the characters from the rest of the elements in the stacks array at that index */
+    stacks[0] = stacks[0].map((char, index)=>{
+        if(!isNaN(parseInt(char))) {
+            return stacks.map((stack)=>stack[index])
+        }
+    })
+    console.log(stacks)
+    /* remove all the undefined elements from the first element of the stacks array */
+    stacks[0] = stacks[0].filter((char)=>char !== undefined)
+    /*copy the first element in stacks to a new array */
+    let gameStacks = stacks[0].slice();
+    /* pop elements from each gameStack in gameStacks that are empty strings */
+    gameStacks.map((stack)=>{
+        while(stack[stack.length-1] === ' ') {
+            stack.pop();
+        }   
+    })
+    /* shift each gameStack once */
+    gameStacks.map((stack)=>{
+        stack.shift();
+    })
+
+    // debugger;
+    /* for each element in the commands array, split the element into an array of characters */
+    commands.map((command, index)=>{
+        commands[index] = command.split('');
+    })
+    /* for each element in the commands array, keep only numbers */
+    commands.map((command, index)=>{
+        commands[index] = command.filter((char)=>!isNaN(parseInt(char)));
+    })
+
+
+    /* for each element in the commands array, convert each character in the element to a number */
+    commands.map((command, index)=>{
+        commands[index] = command.map((char)=>parseInt(char));
+    })
+    /* in commands , subtract 1 from the last element and the one before it */
+    commands.map((command, index)=>{
+        command[command.length-1] -= 1;
+        command[command.length-2] -= 1;
+    })
+
+    /* for each element in the commands array,  create an object where key to is the last element, key from is the one bofore it and key amount is the rest of the elements joined together */
+    commands.map((command, index)=>{
+        commands[index] = {
+            to: command[command.length-1],
+            from: command[command.length-2],
+            amount: Number(command.slice(0,command.length-2).join(''))
+        }
+    })
+    /* for each element in the commands array, create an array where the first element is the amount, the second is the from and the third is the to */
+    commands.map((command, index)=>{
+        commands[index] = [command.amount, command.from, command.to];
+    })
+    /* for each command in the commands array, for the amount stated in the first element in it, move an elemnt from the gameStack at the index stated at the second element to the index stated at the third element*/
+    commands.map((command)=>{
+        for(let i = 0; i < command[0]; i++) {
+            if(command[1]>8 || command[2]>8) { console.log(command[1]>8,command[2]>8);debugger;}
+            if(!gameStacks[command[1]]) debugger;
+            if(gameStacks[command[1]].length !== 0) gameStacks[command[2]].push(gameStacks[command[1]].pop());
+        }
+    })
+    /* combine a string out of the last character element in each gameStacks element */
+    let string = gameStacks.map((stack)=>stack[stack.length-1]).join('');
+    /* log the string */
+    console.log(string)
+    return string;
+
+    /* log the stacks array */
+    console.log(gameStacks)
+}
--- src/days/2022/day9/day9script.ts
+++ src/days/2022/day9/day9script.ts
@@ -1,5 +1,7 @@
 
     import {exampleArray, inputArray} from './day9input'
+    import {createMatrix} from '../../../utils/utils'
+
 export async function script1(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
@@ -9,12 +11,184 @@ export async function script1(useExample: boolean) {
 export async function script2(useExample: boolean) {
     const usedInput = useExample ? exampleArray : inputArray;
     let _res 
-    _res = run(usedInput);
+    _res = run2(usedInput);
     return _res;
 }
 
 export const run = (input) => {
+    console.log(input)
+    let i = 1000;
+    let j = 1000;
+    const map = createMatrix(i,j,'.');
+    let start = [i/2,j/2];
+    let currentH = start.slice();
+    let currentT = start.slice();
+    map[currentT[0]][currentT[1]] = 'T';
+    let sizeEstimation =  {i:0,j:0};
+    let maxI = 0;
+    let maxJ = 0;
+    let commands = [];
+    /* for each element in the input push the first element into commands the amount of times in the second element */
+    input.map((command)=>{
+        for(let i = 0; i < command[1]; i++) {
+            switch(command[0]) {
+                case 'R':
+                    sizeEstimation.j++
+                    break;
+                case 'L':
+                    sizeEstimation.j--
+                    break;
+                case 'U':
+                    sizeEstimation.i++
+                    break;
+                case 'D':
+                    sizeEstimation.i--
+                    break;
+            }
+            maxI = Math.max(maxI, Math.abs(sizeEstimation.i));
+            maxJ = Math.max(maxJ, Math.abs(sizeEstimation.j));
+            commands.push(command[0]);
+        }
+    }
+    )
+    // console.log(maxI,maxJ,sizeEstimation, commands)
+    /* starting at the starting position, for each command in commands, move currentH in the direction of the command */
+    commands.map((command)=>{
+        if(command === 'U') debugger;
+        console.log(command)
+        switch(command) {
+            case 'R':
+                currentH[1]++;
+                // currentT[1]++;
+                break;
+            case 'L':
+                currentH[1]--;
+                // currentT[1]--;
+                break;
+            case 'U':
+                currentH[0]--;
+                // currentT[0]--;
+                break;
+            case 'D':
+                currentH[0]++;
+                // currentT[0]++;
+                break;
+            }
+            //map[currentH[0]][currentH[1]] = 'H';
+        /*if currentT is not adjacent to currentH, move currentT in the direction of currentH */
+        while(!isAdjacent(currentH,currentT)) {
+            if(currentT[0] > currentH[0]) {
+                currentT[0]--;
+            } if(currentT[0] < currentH[0]) {
+                currentT[0]++;
+            } if(currentT[1] > currentH[1]) {
+                currentT[1]--;
+            } if(currentT[1] < currentH[1]) {
+                currentT[1]++;
+            }
+            map[currentT[0]][currentT[1]] = 'T';
+        }
+        console.log(map);
+    }
+    )
+    /* for each element in the map, if it is a T, count it */
+    let count = 0;
+    map.map((row)=>{
+        row.map((element)=>{
+            if(element === 'T') {
+                count++;
+            }
+        })
+    }
+    )
+    return count;
+}
+export const run2 = (input) => {
+    console.log(input)
+    let i = 1000;
+    let j = 1000;
+    const map = createMatrix(i,j,'.');
+    let start = [i/2,j/2];
+    let rope = [
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+        [i/2,j/2],
+    ]
 
+    // console.log(rope)
+    map[rope[9][0]][rope[9][1]] = 'T';
+    let commands = [];
+    /* for each element in the input push the first element into commands the amount of times in the second element */
+    input.map((command)=>{
+        for(let i = 0; i < command[1]; i++) {
+            commands.push(command[0]);
+        }
+    }
+    )
+    /* for each command in commands, move the first element in the rope in the direction of the command */
+    commands.map((command)=>{
+        switch(command) {
+            case 'R':
+                rope[0][1]++;
+                break;
+            case 'L':
+                rope[0][1]--;
+                break;
+            case 'U':
+                rope[0][0]--;
+                break;
+            case 'D':
+                rope[0][0]++;
+                break;
+            }
+        /* for each element in the rope, if it is not adjacent to the element before it, move it in the direction of the element before it */
+        rope.map((element, index)=>{
+            if(index === 0) { return;}
+            while(!isAdjacent(rope[index-1],element)) {
+                if(element[0] > rope[index-1][0]) {
+                    element[0]--;
+                } if(element[0] < rope[index-1][0]) {
+                    element[0]++;
+                } if(element[1] > rope[index-1][1]) {
+                    element[1]--;
+                } if(element[1] < rope[index-1][1]) {
+                    element[1]++;
+                }
+                // map[element[0]][element[1]] = (index)+'';
+                if(index === 9) {
+                    map[element[0]][element[1]] = 'T';
+                }
+            }
+        }
+        )
+        // console.log(map);
+        // debugger;
+    })
+    console.log(map);
+    /* for each element in the map, if it is a T, count it */
+    let count = 0;
+    map.map((row)=>{
+    row.map((element)=>{
+        if(element === 'T') {
+            count++;
+        }
+    })
+    }
+    )
+    return count;
 }
 
-      
+const isAdjacent = (pos1:number[], pos2:number[]):boolean => {
+    /* return true if the two positions are adjacent horizontally, vertically or diagonally */
+    // console.log(pos1,pos2)
+    // console.log((Math.abs(pos1[0] - pos2[0]) <= 1 , Math.abs(pos1[1] - pos2[1]) <= 1))
+
+    return (Math.abs(pos1[0] - pos2[0]) <= 1 && Math.abs(pos1[1] - pos2[1]) <= 1)
+}
